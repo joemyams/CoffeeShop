f8d81bf782c2a0fa9bc6daf7ea15b0fee528cd10
joemyams <joemyams@yahoo.com>
2015-03-16 14:52:24 -0700

    coffee updated

---
+++ www/listings/listings-controller.js
@@ -0,0 +1,52 @@
+(function () {
+    "use strict";
+
+    angular.module("coffee")
+        .controller("ListingsController", ['$scope', 'ListingsService', 'LocationService',
+            function ($scope, ListingsService, LocationService) {
+                var currentPage = 0;
+                $scope.showingListings = true;
+                $scope.canShowMore = true;
+                $scope.listings = [];
+
+                $scope.setShowingListings = function (nextState) {
+                    $scope.showingListings = nextState;
+                };
+
+                function init() {
+                    ListingsService.getListings(currentPage).then(function (listings) {
+                        $scope.listings = listings;
+                        currentPage++;
+                    });
+                }
+
+                $scope.refresh = function () {
+                    $scope.listings = [];
+                    currentPage = 0;
+
+                    LocationService.init();
+                    LocationService.getCurrentPosition().then(function (position) {
+                        init();
+                    })
+                        .finally(function () {
+                            // Stop the ion-refresher from spinning
+                            $scope.$broadcast('scroll.refreshComplete');
+                        })
+                };
+
+                $scope.loadMore = function () {
+                    ListingsService.getListings(currentPage).then(function (listings) {
+                        console.log("Listings: " + listings.length);
+                        if(listings.length === 0){
+                            $scope.canShowMore = false;
+                        }else {
+                            $scope.listings = $scope.listings.concat(listings);
+                            currentPage++;
+                        }
+                        $scope.$broadcast('scroll.infiniteScrollComplete');
+                    });
+                };
+
+                init();
+            }]);
+}());
